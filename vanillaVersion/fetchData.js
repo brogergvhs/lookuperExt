5fff95002177680ff1d21c237235159254240440
Illya Bakhmet-Smolensky <36773220+brogergvhs@users.noreply.github.com>
2023-11-09 19:14:04 +0100

    moved api creds to env

--- vanillaVersion/fetchData.js
+++ vanillaVersion/fetchData.js
@@ -4,25 +4,7 @@ var fetchDataFunc = function () {
     console.log("api:", apiKey, "host:", host);
 
     var data = {
-        urlWordApi: 'https://wordsapiv1.p.rapidapi.com/words/',
-        optionsWordApi: {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': apiKey,
-                'X-RapidAPI-Host': host
-            }
-        },
-
-        urlJpApi: 'https://japanese-text-analysis.p.rapidapi.com/',
-        optionsJpApi: {
-            method: 'POST',
-            headers: {
-                'x-rapidapi-key': 'ee641ba247msh00f2b77313afa28p1468e4jsnce0cfd55e608',
-                'x-rapidapi-host': 'japanese-text-analysis.p.rapidapi.com',
-                'Content-Type': 'application/json'
-            },
-            // max kanji rank 13107
-        }
+        // check .env file for whats supposed to be here
     };
 
     return data;
@@ -30,30 +12,22 @@ var fetchDataFunc = function () {
 
 export { fetchDataFunc };
 
-const url = 'https://japanese-text-analysis.p.rapidapi.com/';
-        const options = {
-            method: 'POST',
-            headers: {
-                'x-rapidapi-key': 'ee641ba247msh00f2b77313afa28p1468e4jsnce0cfd55e608',
-                'x-rapidapi-host': 'japanese-text-analysis.p.rapidapi.com',
-                'Content-Type': 'application/json'
-            },
-        };
 
-        async function getShit() {
-            let req = JSON.stringify({
-                operationName: "analyseText",
-                query: "query analyseText($text: String) {analysis(text: $text) {tokens {entry {word {literal __typename} __typename} pronunciation text __typename} words {literal frequency entries {reading senses {translations __typename} __typename} __typename} kanjis {literal frequency meanings reading_stats {romaji __typename} __typename} __typename}}",
-                variables: {
-                    text: "昨日すき焼きを食べました"
-                }
-            })
-            options.body = req;
-            try {
-                const response = await fetch(url, options);
-                const result = await response.text();
-                console.log(result);   
-            } catch (error) {
-                console.error(error);
-            }
-        }
+// CALL JP 
+// async function getShit() {
+//     let req = JSON.stringify({
+//         operationName: "analyseText",
+//         query: "query analyseText($text: String) {analysis(text: $text) {tokens {entry {word {literal __typename} __typename} pronunciation text __typename} words {literal frequency entries {reading senses {translations __typename} __typename} __typename} kanjis {literal frequency meanings reading_stats {romaji __typename} __typename} __typename}}",
+//         variables: {
+//             text: "昨日すき焼きを食べました"
+//         }
+//     })
+//     options.body = req;
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.text();
+//         console.log(result);   
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
