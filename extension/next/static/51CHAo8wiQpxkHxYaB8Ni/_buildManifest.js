b5e8812d98dc1523fc36b20a0342bdc0e9a5882e
Johannes Frieß <johannes@friess.studio>
2023-11-13 17:06:49 +0100

    initialize background script

--- extension/next/static/51CHAo8wiQpxkHxYaB8Ni/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/d64684d8-ed0abce8092f9d1f.js","static/chunks/pages/index-abf93f5834bc4f69.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- next-app/scripts/background.js
+++ next-app/scripts/background.js
@@ -3,14 +3,14 @@ import { EventEmitter } from '../lib/eventEmitter.js';
 
 
 function init() {
-
-    const messenger = new Messenger();
-    const eventEmitter = new EventEmitter();
-
-    eventEmitter.registerEvent("test");
-
     console.log('background.js: init()');
-    chrome.runtime.onMessage.addListener(onMessage);
+
+    // const messenger = new Messenger();
+    // const eventEmitter = new EventEmitter();
+
+    // eventEmitter.registerEvent(["test"]);
+
+    
 }
 
 init();
